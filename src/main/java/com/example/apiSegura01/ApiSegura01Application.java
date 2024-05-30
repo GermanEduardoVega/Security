75ef5b8e3a3edc594c40a0a6b88054aca22717d7
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2024-05-30 12:26:34 -0300

    dependencias cargadas
    dependencias en gradle

---
+++ src/main/java/com/example/apiSegura01/ApiSegura01Application.java
@@ -0,0 +1,13 @@
+package com.example.apiSegura01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSegura01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSegura01Application.class, args);
+	}
+
+}
